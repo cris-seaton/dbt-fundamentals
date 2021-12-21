66dde998571933231dce614dca286cde0f454dde
Cris T Seaton <c.seaton@montrealanalytics.com>
2021-12-21 19:30:38 +0000

    documentation

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,22 +1,19 @@
+--refactored from dim_customers.sql.old
+
 with customers as (
     select * from {{ ref ('stg_customers') }}
 ),
 orders as (
-  select * from {{ ref ('stg_orders') }}
+  select * from {{ ref ('fct_orders') }}
 ),
-payments as (
-  select
-    customer_id,
-    sum(amount) as lifetime_amount
- from {{ ref ('fct_orders') }}
- group by 1
-),
+
 customer_orders as (
   select
     customer_id,
     min(order_date) as first_order_date,
     max(order_date) as most_recent_order_date,
-    count(order_id) as number_of_orders
+    count(order_id) as number_of_orders,
+    sum(amount) as lifetime_amount
   from orders
   group by 1
 ),
@@ -29,10 +26,9 @@ final as (
     customer_orders.first_order_date,
     customer_orders.most_recent_order_date,
     coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-    payments.lifetime_amount
+    customer_orders.lifetime_amount
   from customers
   left join customer_orders using (customer_id)
-  left join payments using (customer_id)
   order by 1
 )
 
---
+++ models/staging/stripe/stripe.md
@@ -0,0 +1,16 @@
+{% docs payment_method %}
+
+One of the following values:
+
+|  status          |  definition                                   |
+|------------------|-----------------------------------------------|
+|  credit_card     |  VISA, AMEX, Discovery                        |
+|  bank_transfer   |  TD, RBC, Scotiabank, Credit Union            |
+|  coupon.         |  Store recognized coupon                      | 
+|  insert          |  Store recognized gift card                   |
+
+
+
+{% enddocs %}
+
+credit_cardbank_transfercoupongift_card
