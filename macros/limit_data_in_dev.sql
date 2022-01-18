ae560af98f8315d9eac696964f9050e2fa3c0125
Cris T Seaton <c.seaton@montrealanalytics.com>
2022-01-18 19:58:39 +0000

    add default for target.name in macro limit_data...

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,7 @@
 {% macro limit_data_in_dev(loaded_at_field,cutoff_day=3) -%}
 {# {%- if target.name == 'dev' -%} #}
+
+{%- if target.name == 'default' -%}
 where {{ loaded_at_field }} >= date_sub(current_date, interval {{ cutoff_day }} day)
-{#{%- endif -%}#}
-{%- endmacro %}
+{%- endif -%}
+{%- endmacro -%}
